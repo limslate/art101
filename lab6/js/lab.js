a8ed35afbe692081361399221e7b607f5a2c63c4
limslate <limslate@ucsc.edu>
2025-04-28 15:52:25 -0700

    lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,10 +1,58 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Car js updated
+// Author: Emmet Slater
+// Date: 2/28/25
 
-// Constants
 
-// Functions
+//All modes of transport
+myTransport = ["Ford Taurus", "bike", "Ride from friends", "Lime scooter"];
+
+//Collection of my car's attributes
+var myMainRide = {
+  make: "Ford",
+  model: "Taurus",
+  color: "Rust",
+  year: 1995,
+  //Age calculation function embedded in object using year from above.
+  age: function() {
+    return 2025 - this.year;
+  }
+}
+
+
+//Output
+document.writeln("Kinds of transport that I use: ", myTranport, "</br>");
+
+document.writeln("My main ride: <pre>",
+  JSON.stringify(myMainRide, null, '\t'), "</pre>"
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
