8a8ce58664df12a23fb3a63d3f97a4978d936fd1
limslate <limslate@ucsc.edu>
2025-05-15 12:21:50 -0700

    fu

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -15,10 +15,9 @@ function generateRandomText() {
 
 // click listener for button
 $("#make-convo").click(function(){
+    // get new fake dialogue
+    const newText = generateRandomText();
+
+    // append a new div to our output div
+    $("#output").append('<div class="text"><p>' + newText + '</p></div>');
 });
-
-// get new fake dialogue
-const newText = generateRandomText();
-
-// append a new div to our output div
-$("#output").append('<div class="text"><p>' + newText + '</p></div>');
