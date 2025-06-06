bb472168350f18a688767e8b9697b3c087e695f2
limslate <limslate@ucsc.edu>
2025-06-05 18:39:47 -0700

    Update lab.js

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -2,26 +2,60 @@
 // Author: Emmet Slater
 // Date: 6/5/25
 
-$.ajax({
-    url: "https://xkcd.com/info.0.json",
-    data: { 
-       
-    },
+var URLpre = "https://xkcd.com/";
+var URLpost = "info.0.json";
 
-    type: "GET",
-    dataType: "json",
 
-    success: function(comicObj) {
-        console.log("Success!", comicObj);
-        let comicTitle = `<h2>${comicObj.title}</h2>`;
-        let comicImage = `<img src="${comicObj.img}" alt="${comicObj.alt}" title="${comicObj.alt}">`;
-        let comicCaption = `<p>${comicObj.alt}</p>`;
-        $("#output").html(comicTitle + comicImage + comicCaption);
-    },
+function make_safe(str) {
+  return str.replace(/'/g, '&apos;').replace(/"/g, '&quot;');
+}
 
-    error: function(jqXHR, textStatus, errorThrown) { 
-        console.log("Error:", textStatus, errorThrown);
-        $("#output").html(`<p>Something went wrong. Please try again later.</p>`);
-    }
-});
+
+function getComic(num) {
+  let numStr = (typeof num === 'undefined') ? "" : num + "/";
+  let ourURL = URLpre + numStr + URLpost;
+
+  console.log("Fetching:", ourURL);
+
+  $.ajax({
+      url: ourURL,
+      data: {},
+      type: "GET",
+      dataType: "json",
+
+      success: function(data) {
+          let title = make_safe(data.title);
+          let alt = make_safe(data.alt);
+          let imageUrl = data.img;
+          let comicNum = data.num;
+
+          let html = `
+            <div id="imageblock">
+              <h2>${title}</h2>
+              <img src="${imageUrl}" title="${alt}" alt="${alt}">
+              <p>${alt}</p>
+              <button id="prev">Previous</button>
+              <button id="next">Next</button>
+            </div>
+          `;
+
+          $("#output").html(html);
+          $("#prev").click(function() {
+              getComic(comicNum - 1);
+          });
+
+          $("#next").click(function() {
+              getComic(comicNum + 1);
+          });
+      },
+
+      error: function(jqXHR, textStatus, errorThrown) {
+          console.log("Error:", textStatus, errorThrown);
+          $("#output").html(`<p>Something went wrong. Try again later.</p>`);
+      }
+  });
+}
+
+getComic();
+
 
