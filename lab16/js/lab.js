a4f2c489b250fb514c6a7290b26cdbfacfed4950
limslate <limslate@ucsc.edu>
2025-06-05 18:24:22 -0700

    Update lab.js

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -2,24 +2,26 @@
 // Author: Emmet Slater
 // Date: 6/5/25
 
+$.ajax({
+    url: "https://xkcd.com/info.0.json",
+    data: { 
+       
+    },
 
-function getAndDisplayComic() {
-    $.ajax({
-        url: "https://xkcd.com/info.0.json",  
-        type: "GET",
-        dataType: "json",
-        success: function(comicObj) {
-            console.log("Success!", comicObj); 
+    type: "GET",
+    dataType: "json",
 
-            let comicTitle = `<h2>${comicObj.title}</h2>`;
-            let comicImage = `<img src="${comicObj.img}" alt="${comicObj.alt}" title="${comicObj.alt}">`;
-            let comicCaption = `<p>${comicObj.alt}</p>`;
+    success: function(comicObj) {
+        console.log("Success!", comicObj);
+        let comicTitle = `<h2>${comicObj.title}</h2>`;
+        let comicImage = `<img src="${comicObj.img}" alt="${comicObj.alt}" title="${comicObj.alt}">`;
+        let comicCaption = `<p>${comicObj.alt}</p>`;
+        $("#output").html(comicTitle + comicImage + comicCaption);
+    },
 
-            $("#output").html(comicTitle + comicImage + comicCaption);
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.log("Error:", textStatus, errorThrown);
-            $("#output").html(`<p>Something went wrong. Please try again later.</p>`);
-        }
-    });
-}
+    error: function(jqXHR, textStatus, errorThrown) { 
+        console.log("Error:", textStatus, errorThrown);
+        $("#output").html(`<p>Something went wrong. Please try again later.</p>`);
+    }
+});
+
